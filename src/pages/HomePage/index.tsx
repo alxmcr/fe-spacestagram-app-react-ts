fe8f5dada18841511b332bbaa85725876917f926
Alejandro M. Coca <alxmcr@outlook.com>
2021-09-11 00:08:36 -0400

    Add AstronomyImage component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -4,6 +4,8 @@ import "./HomePage.scss"
 export const HomePage = () => {
     return (
         <div className="home">
+            <h1 className="home__title">Spacestagram</h1>
+            <h2 className="home__subtitle">Brought to you by NASA's image API</h2>
             <AstronomyImage />
         </div>
     )
